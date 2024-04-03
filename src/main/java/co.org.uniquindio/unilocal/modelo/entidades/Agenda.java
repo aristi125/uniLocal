6f739df7be7d94921087cc9717f15c50f3c85bf2
Juanes-Quin <juanquinrod@gmail.com>
2024-04-03 13:08:57 -0500

    generar PDF

--- src/main/java/co.org.uniquindio/unilocal/modelo/entidades/Agenda.java
+++ src/main/java/co.org.uniquindio/unilocal/modelo/entidades/Agenda.java
@@ -1,4 +1,4 @@
-package co.org.uniquindio.unilocal.entidades;
+package co.org.uniquindio.unilocal.modelo.entidades;
 
 import lombok.*;
 
--- src/main/java/co.org.uniquindio/unilocal/servicios/interfaces/NegocioServicio.java
+++ src/main/java/co.org.uniquindio/unilocal/servicios/interfaces/NegocioServicio.java
@@ -1,4 +1,10 @@
 package co.org.uniquindio.unilocal.servicios.interfaces;
 
+import co.org.uniquindio.unilocal.dto.Negocio.ReporteDTO;
+
+import java.io.IOException;
+
 public interface NegocioServicio {
+
+    void generarPDF(ReporteDTO reporteDTO, String rutaArchivo) throws IOException;
 }
