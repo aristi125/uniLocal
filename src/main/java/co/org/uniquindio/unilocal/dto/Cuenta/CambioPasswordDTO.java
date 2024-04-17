403b3bd306abced8aa996c4f7a7adf518a229b7d
SirJoao444 <144991198+SirJoao444@users.noreply.github.com>
2024-04-16 20:08:42 -0500

    Update CambioPasswordDTO.java

--- src/main/java/co/org/uniquindio/unilocal/dto/Cuenta/CambioPasswordDTO.java
+++ src/main/java/co/org/uniquindio/unilocal/dto/Cuenta/CambioPasswordDTO.java
@@ -3,7 +3,7 @@ package co.org.uniquindio.unilocal.dto.Cuenta;
 public record CambioPasswordDTO(
       @NotBlank String id,
       @NotBlank @Length (min=8, message="La contraseña debe tener minim 8 caracteres") String passwordNueva,
-      @Email    String email,
+      @NotBlank @Email    String email,
                 String token
 ) {
 }
