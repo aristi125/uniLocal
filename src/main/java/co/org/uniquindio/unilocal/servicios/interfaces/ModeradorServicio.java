116dea8337d27a951c62457dcd1de3f6af313874
Juanes-Quin <juanquinrod@gmail.com>
2024-05-23 10:20:08 -0500

    Login

--- src/main/java/co/org/uniquindio/unilocal/servicios/interfaces/ModeradorServicio.java
+++ src/main/java/co/org/uniquindio/unilocal/servicios/interfaces/ModeradorServicio.java
@@ -2,16 +2,8 @@ package co.org.uniquindio.unilocal.servicios.interfaces;
 
 
 import co.org.uniquindio.unilocal.dto.Moderador.DetalleModeradorDTO;
-import co.org.uniquindio.unilocal.dto.Moderador.RevisionesModeradorDTO;
-import co.org.uniquindio.unilocal.dto.cliente.DetalleClienteDTO;
-import co.org.uniquindio.unilocal.dto.comentario.RevisarComentariosDTO;
 import co.org.uniquindio.unilocal.dto.cuenta.CambioPasswordDTO;
 import co.org.uniquindio.unilocal.dto.cuenta.LinkRecuperacionDTO;
-import co.org.uniquindio.unilocal.dto.negocio.ItemNegociosRevisionDTO;
-import co.org.uniquindio.unilocal.modelo.documentos.Cliente;
-import co.org.uniquindio.unilocal.modelo.documentos.HistorialRevision;
-import co.org.uniquindio.unilocal.modelo.documentos.Moderador;
-import co.org.uniquindio.unilocal.modelo.enumeracion.EstadoNegocio;
 
 import java.util.List;
 
